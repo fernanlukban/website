a00c982fc7a73ef21406d445f913e231614a5499
Fernan Lukban <fernanlukban@gmail.com>
2022-02-01 00:34:57 -0800

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `opsadboys`,
+    siteTitleAlt: `opsadboys`,
+    siteHeadline: `my life`,
+    siteUrl: `https://opsadboys.com`,
+    siteDescription: ``,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteImage: ``,
+    author: `@opsadboys`,
   },
   plugins: [
     {
@@ -30,15 +30,19 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Recipes`,
+            slug: `/recipes`,
+          }
         ],
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/opsadboys`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Instagram`,
+            url: `https://instagram.com/fernanlukban`,
           },
         ],
       },
