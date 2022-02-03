6a0b2cd91c9e20d756291f07fba7db51df62b849
Fernan Lukban <fernanlukban@gmail.com>
2022-02-02 23:47:47 -0800

    added new stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,13 @@ exports.createSchemaCustomization = ({ actions }) => {
     const { createFieldExtension, createTypes } = actions
 
     createTypes(`
+        type Ingredient implements Node {
+            name: String!
+        }
         type Recipe implements Node {
-            title: String
+            title: String!
+            step(number: Int!): String
+            ingredients: [Ingredient!]!
         }
     `)
 }
